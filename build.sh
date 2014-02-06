d72e584c0faeb8003c149838bd06abba066d8a25
Sergey V. Yudin <tsipa740@gmail.com>
2014-02-06 12:59:26 +0000

    install is ok

--- build.sh
+++ build.sh
@@ -2,18 +2,21 @@
 
 shebang='#!/usr/bin/env python'
 
-target="./troublestack_`date +%s`.py"
+#target="./troublestack_`date +%s`.py"
+target="./troublestack_tgt.py"
 
 
 echo "${shebang}" > ${target}
+echo 'basefile = {}' >> ${target}
 chmod oug+x "${target}"
 
 
 for f in ./files/* ; do
   fn=`basename ${f}`
   line="basefile['${fn}']"
-  line="${line}='''`base64 ${f}`'''"
+  line="${line}=\"\"\"`base64 ${f}`\"\"\""
   echo "${line}" >> "${target}"
 done
 
+cat extractor.py >> ${target}
 cat troublestack.py >> ${target}
--- troublestack.py
+++ troublestack.py
@@ -1,5 +1,50 @@
+import re
+import os
+import commands
+import json
 
+class Platform():
+  def __init__(self):
+    if not os.system('gem list | grep ohai &>/dev/null') == 0 and not os.system('gem install ohai') == 0 and not os.system('gem install /var/www/nailgun/gems/gems/ohai*.gem') == 0:
+      exit(1)
+    self.raw_data = json.loads(commands.getoutput('ohai'))
 
-
-def extract():
+  def pkglocalprovider(self):
+    if self.getfamily() == "debian":
+      return "dpkg -i"
+    elif self.getfamily() == "redhat":
+      return "rpm -ivh"
   
+  def getpkgarch(self):
+    if re.search('64', self.raw_data['kernel']['machine'], re.IGNORECASE):
+      if self.getfamily() == "debian":
+        return "amd64"
+      elif self.getfamily() == "redhat":
+        return "x86_64"
+    elif re.search('i[0-9]86', self.raw_data['kernel']['machine'], re.IGNORECASE):
+      if self.getfamily() == "debian":
+        return "i686"
+      elif self.getfamily() == "redhat":
+        return "i386"
+
+  def getpkgextension(self):
+    if self.getfamily() == "debian":
+      return "deb"
+    elif self.getfamily() == "redhat":
+      return "rpm" 
+
+  def getfamily(self):
+    if re.search('ubuntu', self.raw_data['platform'], re.IGNORECASE) or re.search('debian', self.raw_data['platform'], re.IGNORECASE) or re.search('debian', self.raw_data['platform_family'], re.IGNORECASE):
+      return "debian"
+    elif re.search('centos', self.raw_data['platform'], re.IGNORECASE) or re.search('redhat', self.raw_data['platform'], re.IGNORECASE) or re.search('rhel', self.raw_data['platform_family'], re.IGNORECASE):
+      return "redhat"
+    else:
+      return "debian"
+
+  def install(self, pkg):
+    install_cmd=self.pkglocalprovider() + ' /tmp/' + pkg + '*' + self.getpkgarch() + '*.' + self.getpkgextension()
+    if not os.system(install_cmd) == 0:
+      print install_cmd, 'failed'
+
+me = Platform()
+me.install("atop")
