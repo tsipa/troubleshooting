3a7e7c80a35ba53e7d2105fcbb7f6b82f411745c
Sergey V. yudin <tsipa740@gmail.com>
2014-02-26 10:58:23 -0500

    astute yaml also needed

--- files/controller/get_logs.py
+++ files/controller/get_logs.py
@@ -1,3 +1,3 @@
-needed_files = ['/etc/openvswitch/', '/etc/corosync/', '/var/log/', '/etc/my.cnf', '/etc/mysql/', '/etc/fuel-uuid', '/etc/naily.facts', '/etc/nailgun/', '/etc/naily/', '/etc/haproxy/' ]
+needed_files = ['/etc/openvswitch/', '/etc/astute.yaml', '/etc/corosync/', '/var/log/', '/etc/my.cnf', '/etc/mysql/', '/etc/fuel-uuid', '/etc/naily.facts', '/etc/nailgun/', '/etc/naily/', '/etc/haproxy/' ]
 for file in needed_files:
   pool.get_fromrole('all', file)
